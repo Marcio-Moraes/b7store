7f8fa852e32d0a83ed68e0eb4098603178b70dba
Marcio Moraes <marciomoraes.cdd@gmail.com>
2025-09-16 13:45:56 -0300

    Component FiltroItem

--- src/components/categories/filtro-item.tsx
+++ src/components/categories/filtro-item.tsx
@@ -1,5 +1,7 @@
 "use client"
 
+import { useQueryString } from "@/hooks/use-querystring";
+
 type Props = {
     grupoId: string;
     item: {
@@ -9,10 +11,21 @@ type Props = {
 }
 
 export const FiltroItem = ({grupoId, item}: Props) => {
+    const queryString = useQueryString();
+
+    const toggleFiltro = (grupoId: string, itemId: string) => {
+
+    }
+
     return(
         <div className="flex items-center gap-4 mt-4">
             <div className="">
-                <input type="checkbox" className="size-6" id={`ck-${item.id}`} />
+                <input 
+                    type="checkbox" 
+                    className="size-6" 
+                    id={`ck-${item.id}`} 
+                    onChange={()=> toggleFiltro(grupoId, item.id)}
+                />
             </div>
             <label htmlFor={`ck-${item.id}`} className="text-lg text-gray-500">
                 {item.label}
