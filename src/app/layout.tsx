240374390ba79328c1288f97ece0207bc49499f2
Marcio Moraes <marciomoraes.cdd@gmail.com>
2026-06-02 16:12:46 -0300

    Metadatas

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,9 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "B7store"
+  metadataBase: new URL("https://b7store.com.br"),
+  title: "B7Store",
+  description: "B7Store - A melhor loja de roupas e acessórios para desenvolvedores. Camisetas, bonés e muito mais.",
 };
 
 export default function RootLayout({
@@ -12,7 +14,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-br">
-      <body>
+      <body className="vc-init">
         {children}
       </body>
     </html>
