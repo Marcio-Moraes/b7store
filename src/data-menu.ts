3d00890246283182ac7f839cc05585153fcf2e1a
Marcio Moraes <marciomoraes.cdd@gmail.com>
2026-06-02 16:10:33 -0300

    Atualização data-menu

--- src/data-menu.ts
+++ src/data-menu.ts
@@ -2,8 +2,9 @@ import { MenuItem } from "./type/menu-item";
 
 
 export const menu: MenuItem[] = [
-    { label: 'Camisa', href: '/categories/camisa'},
-    { label: 'Kits', href: '/categories/kits'},
+    { label: 'Camisa', href: '/categories/camisa'},    
     { label: 'Bonê', href: '/categories/bone'},
     { label: 'Calça', href: '/categories/calca'},
+    { label: 'Moletom', href: '/categories/moletom'},
+    { label: 'Bermuda', href: '/categories/bermuda'},
 ];
