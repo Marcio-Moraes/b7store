d050175e5ba56b2a2a81ac2cbd6daebc52c357a1
Marcio Moraes <marciomoraes.cdd@gmail.com>
2026-06-02 16:10:16 -0300

    Atualizações Type Produto

--- src/type/produto.ts
+++ src/type/produto.ts
@@ -1,7 +1,15 @@
 export type Produto = {
     id: number;
     label: string;
+    slug: string;
+    category: string;
+    description: string;
     img: string;
+    images: string[];
     price: number;
+    priceOriginal: number;
     liked: boolean;
+    sizes: string[];
+    colors: { name: string; hex: string }[];
+    details: string[];
 }
